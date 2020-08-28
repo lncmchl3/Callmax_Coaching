8861b3617c1e739137adcea940c3a78d93ad81d9
lncmchl3 <lm.teodoro.callmax@gmail.com>
2020-08-29 00:06:08 +0800

    asdasd

--- connection.php
+++ connection.php
@@ -2,5 +2,5 @@
 	$connection = mysqli_connect('localhost','root','','callmaxcoaching');
 	if(!$connection){
 		die("Please Check your Connection".mysqli_error($connection));
-	}
-?>
+	} <
+?> <!-- sample -->
