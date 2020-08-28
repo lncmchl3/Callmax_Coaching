90411faf2aaea420055785e7722e4438a796f947
lncmchl3 <lm.teodoro.callmax@gmail.com>
2020-08-29 06:06:38 +0800

    latest login design draft

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 
-  
-<html>
+  <html>
     <head>
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -13,65 +12,68 @@
             if (@is_null($_SESSION['demerits'])) {
         ?>
         <link rel="icon" href="searchicon.png">
-        <div class="containter">
-		<div class="box-1">
-        <form class="my-form" "method="post" action="login.php" autocomplete="off">
-        <!--
-            <center>	
-			<span class="center">Login</span>			
+
+ <!--
+            <center>    
+            <span class="center">Login</span>           
                 <table border="1" width="30%" cellpadding="3">
                     <thead>
                         <tr>
-						<?php
-							if(@$_GET['e']==true){
-						?>
-						<th colspan="2" height="16px" style="color: orange;"><?php echo $_GET['e'] ?></th>
-						<?php
-							}else if (@$_GET['i']==true) {
+                        <?php
+                            if(@$_GET['e']==true){
+                        ?>
+                        <th colspan="2" height="16px" style="color: orange;"><?php echo $_GET['e'] ?></th>
+                        <?php
+                            }else if (@$_GET['i']==true) {
                                 ?>
                                 <th colspan="2" height="16px" style="color: red;"><?php echo $_GET['i'] ?></th>
                                 <?php
                             }
-							else{
-						?>
-						<th colspan="2" height="16px"></th>
-							<?php
-							}
-							?>
+                            else{
+                        ?>
+                        <th colspan="2" height="16px"></th>
+                            <?php
+                            }
+                            ?>
                            
                         </tr>
                     </thead>
                     -->
-                        <div class="form-group">
-                            <h4>LOGIN</h4>
-                        </div>
-                        <div class="form-group">
-                            <label>Username: </label>
-                            <input type="text" name="uname" value="" placeholder="Enter your username" />
-                        </div>
+ 
+        <form class="my-form" method="post" action="login.php" autocomplete="off">
+       
+                        
+                        <h1>LOGIN</h1>
                         
                         <div class="form-group">
-                            <label>Password: </label>
-                            <input type="password" name="pass" value="" placeholder="Enter your username" />
+                            <div class="form-group">
+                                <!-- <label>Username: </label> -->
+                                <img src="images/user.png" width="20px">
+                                <input type="text" name="uname" value="" placeholder="USERNAME" />
+                            </div>
+                            
+                            <div class="form-group">
+                                <!-- <label>Password: </label> -->
+                                <img src="images/pass.png" width="20px">
+                                <input type="password" name="pass" value="" placeholder="PASSWORD" />
+                            </div>
                         </div>
                         
-                        <div class="form-group">
-                            <input class="button" type="reset" value="RESET" />
-                        </div>
-                        
-                        <div class="form-group">
-							<button class= "button "name="Login"/>LOGIN</button>
-                        </div>
+ 
+                            
+    						<button class= "button1" type="submit" name="Login"/>LOGIN</button>
+
+                        <div class="form-group"><a href="#">Forgot password?</a></div>
 
                         <div class="form-group">
-                            <label>Not registered?</label>
-                            <a href="reg.php">Register Here</a>
-                        </div>
-                    
-             
+                            <label>Not a member?</label>
+                            <a href="reg.php">Sign up</a>
+                        </div>  
         </form>
-        </div>
-                        </div>
+
+
+<div class="footer"><p>Copyright &copy; 2020, Callmax Solutions.</p></div>
+
         <?php
             }else if (@$_SESSION['demerits'] == "TL") {
                     header("location:agentSearch/AgentSearch.php");
