f4dfe2159bb1002ebce9c84b6f32a6b064752e3a
lncmchl3 <lm.teodoro.callmax@gmail.com>
2020-09-03 23:48:42 +0800

    dash table table in progress

--- agentSearch/AgentSearch.php
+++ agentSearch/AgentSearch.php
@@ -2,8 +2,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Callmax Solutions Coaching System</title>
+    <title>Callmax Solutions Coaching System | Dashboard</title>
     <link rel="stylesheet" type="text/css" href="../css/html-style.css">
+
 </head>
 <body>
     <?php
@@ -15,7 +16,14 @@
         $choices = @$_POST['item'];
         $search = @$_POST['search'];
         $date = @$_POST['date'];
+
+
     ?>
+
+
+
+
+
 
 <!-- code for content here -->
 <header>
@@ -55,13 +63,20 @@
                     <option value="status">Status</option>
                 </select>
 
-                <input type="text" name="search" placeholder="Search.." >
+                <input type="text" name="search" placeholder="Search" >
                 <input type="date" name="date">
                 <button class="button" type="submit" name="searchForm">Search</button>
+                <?php
+                //set initital value of POST iten to be true to laod the initial table
+                $choices = isset($_POST['item']) ? $_POST['item'] : 1;
+                 ?>
+
           </form>
 
           </div>
           <!-- end of search-formbox -->
+
+
 
       </div>
       <!-- end of subcontainer of search-formbox -->
@@ -70,70 +85,72 @@
       <!-- start of table -->
       <div class="subcontainer">
         <div class="tablebox">
-
         <?php include('search.php'); ?>
         <?php
         if ($choices == null) {
         ?>
-        <br/>
+
         <?php
         }
         else {
         ?>
         <?php
-        include('../connection.php');
+          include('../connection.php');
 
+            for($i = 0; $i < count($data); $i++){
+            $query1 = "SELECT * FROM agent_reports WHERE UPPER(" .$choices. ") LIKE UPPER('%" .$search. "%') AND UPPER(account) LIKE UPPER('".$data[$i]."') AND date_issued LIKE '".$date."%' ORDER BY id DESC,date_issued ASC limit 50";
+            $result1 = mysqli_query($connection, $query1);
+            if (mysqli_num_rows($result1) > 0) {
+        ?>
+          <div class="table_div">
+            <table class="dashboard-table">
+              <thead class="table-heading">
+                <tr>
+                  <th colspan="30"><h3><?php echo $data[$i]; ?></h3></th>
+                </tr>
+              </thead>
 
-                                    for($i = 0; $i < count($data); $i++){
-                                    $query1 = "SELECT * FROM agent_reports WHERE UPPER(" .$choices. ") LIKE UPPER('%" .$search. "%') AND UPPER(account) LIKE UPPER('".$data[$i]."') AND date_issued LIKE '".$date."%' ORDER BY id DESC,date_issued ASC limit 50";
-                                    $result1 = mysqli_query($connection, $query1);
-                                    if (mysqli_num_rows($result1) > 0) {
-                        ?>
-                        <div class="table_div">
-                        <table border = "1" width="100%">
-                            <thead>
-                            <tr>
-                                <th colspan="30"><?php echo $data[$i]; ?></th>
-                            </tr>
-                        </thead>
-                        <thead>
-                            <tr>
-                                <th>Agent's Name</th>
-                                <th>Type of Report</th>
-                                <th>Date Issued</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                                    while ($row1 = mysqli_fetch_assoc($result1)) {
-                            ?>
+              <thead>
+                <tr>
+                  <th>Agent's Name</th>
+                  <th>Type of Report</th>
+                  <th>Date Issued</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
 
-                                <tr onclick="window.location='info.php?n=<?php echo $row1['id']; ?>&type=<?php echo $row1['report_type']; ?>&string=<?php echo $row1['report_id']; ?>&status=<?php echo $row1['status']; ?>'" class="only">
-                                <td><?php echo $row1['agent']; ?></td>
-                                <td style="width: 15%; text-align: center;"> <?php echo $row1['report_type']; ?></td>
-                                <td style="width: 15%; text-align: center;"> <?php echo $row1['date_issued']; ?></td>
-                                <?php
-                                    if($row1['status']=="Pending"){
-                                ?>
-                                <td style="width: 15%; text-align: center; color: black; background-color: #ff6f5c;"><?php echo $row1['status']; ?></td>
-                                <?php }else if($row1['status']=="Submitted"){ ?>
-                                <td style="width: 15%; text-align: center; color: black; background-color: #ffcb5c;"><?php echo $row1['status']; ?></td>
-                                <?php }else{ ?>
-                                <td style="width: 15%; text-align: center; color: black; background-color: #92ff5c;"><?php echo $row1['status']; ?></td>
-                                <?php
-                                    }
-                                }
-                                ?>
-                        </tr>
-                        </tbody>
-                        </table>
-                        </div>
-                            <?php
-                                    }
-                                }
-                            ?>
-                        <?php } ?>
+              <tbody>
+              <?php
+                while ($row1 = mysqli_fetch_assoc($result1)) {
+              ?>
+
+              <tr onclick="window.location='info.php?n=<?php echo $row1['id']; ?>&type=<?php echo $row1['report_type']; ?>&string=<?php echo $row1['report_id']; ?>&status=<?php echo $row1['status']; ?>'" class="only">
+
+                <td><?php echo $row1['agent']; ?></td>
+                <td class="tableData"> <?php echo $row1['report_type']; ?></td>
+                <td class="tableData"> <?php echo $row1['date_issued']; ?></td>
+                <?php
+                if($row1['status']=="Pending"){
+                  ?>
+                  <td class="tableStatusOrange"><?php echo $row1['status']; ?></td>
+                <?php }else if($row1['status']=="Submitted"){ ?>
+                  <td class="tableStatusYellow"><?php echo $row1['status']; ?></td>
+                <?php }else{ ?>
+                  <td class="tableStatusGreen"><?php echo $row1['status']; ?></td>
+                  <?php
+                }
+              }
+              ?>
+            </tr>
+          </tbody>
+          <!-- end of table body -->
+        </table>
+      </div>
+<?php
+}
+}
+?>
+<?php } ?>
 
         </div>
         <!-- end of tablebox -->
@@ -160,9 +177,15 @@
     }
 ?>
 
+
+
+
 <footer>
     <h4>Callmax Solutions Coaching System, Copyright &copy; 2020</h4>
 </footer>
+
+
+
 
 </body>
 </html>
